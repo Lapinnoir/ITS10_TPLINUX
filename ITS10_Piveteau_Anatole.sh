e3b5c3a3a3ca122f699461b044d31fd421d0b377
Wazhabits <anatole.piveteau@gmail.com>
2018-03-02 12:20:17 +0100

    Push de Anatole

---
+++ ITS10_Piveteau_Anatole.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Created by Anatole Piveteau		#
+# at : 02/03/2018 10:46:30		#
+# Environnement : DEBIAN 9.1.0		#
+# mail : anatole.piveteau@gmail.com	#
+#					#
+# BEGIN					#
+#					#
+# change pwd env variable to home	#
+echo "Begin\n"
+echo "Change directory to home\n"
+cd ~/..
+# Create directory			#
+echo "Creating directory:\n\t_ LaGriffe\n\t_ LaGriffe/Gantelet_Infini\n\t_ LaGriffe/Gantelet_Infini\Script\n\t_ JimMorrison\n\t_ Tracer\n\t_ info\n\n"
+mkdir /home/LaGriffe /home/LaGriffe/Gantelet_Infini /home/JimMorrison /home/Tracer /home/LaGriffe/Gantelet_Infini/Script home/info
+# Create user group Overwatch
+echo "Create group Overwatch\n"
+groupadd -f Overwatch
+# Create plan.txt			#
+echo "Create plan.txt in LaGriffe/Gantelet_Infini/plan.txt\n"
+echo "Que Onda hahaha\n" > /home/LaGriffe/Gantelet_Infini/plan.txt
+# Create users				#
+#	- user - pwd			#
+#	- JimMorrison - jimmorrison	#
+#	- Tracer - tracer		#
+#	- Fatale - fatale		#
+echo "Create users :\n\tTracer\n\tJimMorrison\n\tFatale"
+useradd -u 111111 -g Overwatch -d /home/JimMorrison -s /bin/bash -p $(echo jimmorrison | openssl passwd -1 -stdin) JimMorrison
+useradd -u 121212 -g Overwatch -d /home/Tracer -s /bin/bash -p $(echo tracer | openssl passwd -1 -stdin) Tracer
+useradd -u 131313 -d /home/LaGriffe -s /bin/bash -p $(echo fatale | openssl passwd -1 -stdin) Fatale
+# CREATE SCRIPT AND GIVE RIGHT		#
+echo "Create script file blackwatch.sh in LaGriffe/Gantelet_Infini/Script"
+echo 'echo "un tir, un mort."\n'>/home/LaGriffe/Gantelet_Infini/Script/blackwatch.sh
+echo "Create script file Hey!.sh in Tracer/"
+echo 'echo Salut mon chou ! La cavalerie est arrivée\n'>/home/Tracer/Hey!.sh 
+echo "Give access to Fatale to directory Gantelet_Infini\n"
+chown -R Fatale:Fatale /home/LaGriffe/Gantelet_Infini/
+echo "Give Tracer access to Tracer/Hey!.sh script\n"
+chown -R Tracer:Overwatch /home/Tracer/Hey\!.sh
+echo "Give Tracer access to JimMorrison directory\n"
+chown -R Tracer:Overwatch /home/JimMorrison/
+echo "End"
