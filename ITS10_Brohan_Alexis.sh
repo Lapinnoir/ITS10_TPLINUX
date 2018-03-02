8f5fc4892603ad3decff469bd16bb944e0d30dbb
AlexBrh3 <alexisbrohan68@gmail.com>
2018-03-02 15:01:20 +0100

    Script de Alexis

---
+++ ITS10_Brohan_Alexis.sh
@@ -0,0 +1,55 @@
+#Brohan Alexis
+#02/03/18
+#Script fonctionnel sur Linux
+#alexisbrohan68@gmail.com
+
+#Création des users
+useradd JackMorrison -m -s /home/JackMorrison/bash && echo "User JackMorrison crée"
+passwd JackMorrison
+useradd Tracer -m -s /home/Tracer/bash && echo "User Tracer crée"
+passwd Tracer
+useradd Fatale -m -s /home/Fatale/bash && echo "User Fatale crée"
+passwd Fatale
+
+#Création groupe
+addgroup --force-badname Overwatch && echo "Groupe Overwatch crée"
+
+#Ajout de Tracer et JackMorrison dans Overwatch
+usermod -aG Overwatch JackMorrison && usermod -aG Overwatch Tracer && echo "Tracer et JackMorrison ajoutés au groupe Overwatch"
+
+#Création rep LaGriffe/Gantelet_Infini avec plan.txt et /Script/blackwatch.sh
+mkdir -p /home/LaGriffe/Gantelet_Infini && echo "Répertoire LaGriffe/Gantelet_Infini crée"
+cd /home/LaGriffe/Gantelet_Infini
+touch plan.txt && echo "Plan.txt crée avec succès"
+cd ..
+chown -R Fatale:Overwatch /home/LaGriffe/Gantelet_Infini
+chmod 751 /home/LaGriffe/Gantelet_Infini
+cd Gantelet_Infini
+chmod 751 plan.txt
+echo "Que Onda hahaha" > plan.txt && echo "Que Onda hahaha ajouté à plan.txt"
+cd ..
+mkdir Script
+cd Script
+touch blackwatch.sh && echo "blackwatch.sh crée avec succès"
+echo "un tir, un mort" > blackwatch.sh && echo "un tir, un mort"
+chown Fatale blackwatch.sh
+chmod 711 blackwatch.sh
+cd ..
+chown Fatale Script
+chmod 700 Script
+
+#Création Hey!.sh
+cd /home/Tracer
+touch Hey!.sh && echo "Hey!.sh crée avec succès"
+echo "Salut mon choux ! La cavalerie est arrivée !" > Hey!.sh && echo "Insertion dans Hey!.sh réalisée avec succès"
+chown Tracer:Overwatch Hey!.sh
+chmod 500 Hey!.sh
+
+#Accès de Tracer au dossier JackMorrison bloqué
+chown JackMorrison:Overwatch /home/JackMorrison
+chmod 700 /home/JackMorrison
+
+#Création home/info et bloquage de Tracer
+mkdir /home/info && echo "Répertoire /home/info crée avec succès"
+chown :Overwatch /home/info
+chmod 751 /home/info
